0cbd9fcd7d155328ddafd01645aced8edd30dae5
Rupali Bhatnagar <rupalibhatnagar0509@gmail.com>
2025-06-11 16:32:31 +0530

    Power_Of_Gman: init

---
+++ Power_of_GMan/geektrust.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+const filename = process.argv[2]
+
+fs.readFile(filename, "utf8", (err, data) => {
+    /*if (err) throw err
+    var inputLines = data.toString().split("\n")
+    // Add your code here to process input commands
+    */
+})
